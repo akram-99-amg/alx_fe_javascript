bbd790e429a4d978ed3ca1b7466e12b86c760199
Akram <mazakram04@gmail.com>
2025-01-30 16:44:40 -0500

    add json

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,9 +1,12 @@
-const Quotes = [
+const Quotes = JSON.parse(localStorage.getItem("quotes")) || [
     { text: "You only live once, but if you do it right, once is enough.", category: "Motivation" },
     { text: "It is better to be hated for what you are than to be loved for what you are not.", category: "Inspiration" },
     { text: "Without music, life would be a mistake.", category: "Happiness" },
     { text: "If you tell the truth, you don't have to remember anything.", category: "Wellness" },
 ]
+function saveQuotes(){
+    localStorage.getItem(JSON.stringify(Quotes))
+}
 
 function showRandomQuote() {
     const randomIndex = Math.floor(Math.random() * Quotes.length);
